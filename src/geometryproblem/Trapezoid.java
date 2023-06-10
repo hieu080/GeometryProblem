0e71b3efe7420a26e2c61bcfe7368ba43f313b40
unknown <phantrunghieu020103@gmail.com>
2023-06-10 08:47:14 +0700

    first commit

--- src/geometryproblem/Trapezoid.java
+++ src/geometryproblem/Trapezoid.java
@@ -10,24 +10,19 @@ package geometryproblem;
  * @author HELLO
  */
 public class Trapezoid extends Quadrilateral {
-    
-    private double height;
-    
+
     public Trapezoid(Line side1, Line side2, Line side3, Line side4) {
         super(side1, side2, side3, side4);
-        getHeight();
     }
-    
+
     public double getArea() {
-        double sumOfBases = getSide1().getLength() + getSide2().getLength();
-        double area = 0.5 * sumOfBases * height;
+        double a = getSide2().getLength();
+        double b = getSide1().getLength();
+        double c = getSide4().getLength();
+        double d = getSide3().getLength();
+        
+        double area = ((c + a) * Math.sqrt(Math.pow(d, 2) - Math.pow(((Math.pow(b - c, 2) + Math.pow(d, 2) - Math.pow(a, 2)) / (2 * (b - c))), 2))) / 2;
         return area;
     }
-    
-    private void getHeight() {
-        double base1Length = getSide1().getLength();
-        double base2Length = getSide2().getLength();
-        double verticalDistance = getSide3().getLength();
-        height = Math.sqrt(verticalDistance * verticalDistance - Math.pow((base1Length - base2Length) / 2, 2));
-    }
 }
+
